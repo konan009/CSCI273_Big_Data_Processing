b3e087e1306c99d19539bb36dd41e99a30b71295
Meljohn <ugaddanmeljohn@gmail.com>
2024-02-14 19:27:35 +0800

    Approximate Pi using reduce is finalized

--- approximatepi_reduceversion/approximatepi.c
+++ approximatepi_reduceversion/approximatepi.c
@@ -18,12 +18,12 @@ int main(int argc, char *argv[]) {
    float height = 4.0 / (1.0 + x_mid * x_mid);
    sum += height;
 
-   printf( "[Process %.2d] %.4f \n", rank, sum);
+   printf( "[Process %.2d] Sending %.4f to process 0 \n", rank, sum);
    MPI_Reduce(&sum, &reduction_result, 1, MPI_FLOAT, MPI_SUM, ROOT_RANK, MPI_COMM_WORLD);
    
    if ( rank == ROOT_RANK ){
       float pi = reduction_result * delta_x;
-      printf("[Process %.2d] Total Sum : %.10f\n", rank, pi);
+      printf("[Process %.2d] Pi is ≈ %.10f\n", rank, pi);
    }
 
    printf("[Process %.2d] Ended \n", rank );
