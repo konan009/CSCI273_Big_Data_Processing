101c189153465f28debc94830aa1f528442bf3ca
Meljohn <ugaddanmeljohn@gmail.com>
2024-02-14 19:48:49 +0800

    Finalize sum of n using reduce version

--- sum_n_reduceversion/reduce.c
+++ sum_n_reduceversion/reduce.c
@@ -8,18 +8,16 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int my_rank = rank; 
     int reduction_result = 0;
 
-    printf("[Process %.2d] Starting \n",rank);
+    printf("[Process %2d] Starting \n",rank);
 
-    MPI_Reduce(&my_rank, &reduction_result, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-
+    MPI_Reduce(&rank, &reduction_result, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
     if ( rank == 0 ){
-      printf("[Process %.2d] Total Sum : %d\n", rank, reduction_result);
+      printf("[Process %2d] Total Sum : %d\n", rank, reduction_result);
     }
 
-    printf("[Process %.2d] Ended \n", rank );
+    printf("[Process %2d] Ended \n", rank );
     MPI_Finalize();
     return 0;
 }
